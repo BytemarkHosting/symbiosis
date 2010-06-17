eeaa313614e3a82622187959c3befd47339974df
Steve Kemp <steve@steve.org.uk>
2010-06-17 12:39:36 +0100

    removed duplicitous /bin/sh entries.

--- cron/wrapper/symbiosis-crontab.c
+++ cron/wrapper/symbiosis-crontab.c
@@ -149,7 +149,7 @@ int process_domains( const char *dirname )
            }
        }
 
-       printf("system(/bin/su -s /bin/sh -c '/bin/sh ruby /usr/bin/crontab /srv/%s' %s)\n",
+       printf("system(/bin/su -s /bin/sh -c '/usr/bin/crontab /srv/%s' %s)\n",
               entry, pwd->pw_name  );
      }
    closedir(dp);
