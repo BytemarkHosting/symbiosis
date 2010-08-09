1547b9c74f51a4480ab0078bc2b25703872f0211
Steve Kemp <steve@steve.org.uk>
2010-08-09 09:06:43 +0100

    Updated.

--- crack/passwd-test.c
+++ crack/passwd-test.c
@@ -8,6 +8,7 @@
 
 #include <crack.h>
 #include <stdio.h>
+#include <string.h>
 
 
 /*
@@ -21,23 +22,65 @@
 
 
 /**
- * Test the password specified upon the command line.
+ * Test the password stored in the file given upon command line.
  */
 int main( int argc, char *argv[] )
 {
     char const* msg;
+    char line[1024];
+    FILE *f;
     int i;
     int fail = 0;
 
     for ( i = 1; i < argc; i++ )
     {
-        msg = FascistCheck( argv[i], CRACKLIB_DICTPATH );
+        f = fopen( argv[i], "r" );
+        memset(line, '\0',sizeof(line));
 
-        if( msg )
+        if ( f == NULL )
         {
-            printf( "Password '%s' is insecure:\n\t%s\n",
-                    argv[i], msg );
-            fail = 1;
+            fprintf(stderr, "Failed to open file: %s\n", argv[i] );
+        }
+        else
+        {
+            int j = 0;
+
+            /**
+             * Read password.
+             */
+            fgets( line, sizeof(line)-1, f );
+
+            /**
+             * Strip newline(s).
+             */
+            while( line[j] != '\0' )
+            {
+                if ( ( line[j] == '\r' ) || ( line[j] == '\n' ) )
+                    line[j] = '\0';
+
+                j+=1;
+            }
+            fclose( f );
+
+
+            /**
+             * Test password.
+             */
+            if ( strlen( line ) > 0 )
+            {
+                msg = FascistCheck( line, CRACKLIB_DICTPATH );
+
+                if( msg )
+                {
+                    printf( "Password '%s' stored in '%s' is insecure:\n\t%s\n",
+                            line, argv[i], msg );
+                    fail = 1;
+                }
+            }
+            else
+            {
+              fprintf(stderr, "Skipping empty file: %s\n", argv[i] );
+            }
         }
     }
     return fail;
@@ -52,14 +95,15 @@ passwd-test - Test a given password for strength
 
 =head1 SYNOPSIS
 
-  passwd-test password1 password2 ... passwordN
+  passwd-test file1 file2 ... fileN
 
 =cut
 
 =head1 DESCRIPTION
 
-The B<passwd-test> binary is designed to test the password(s)
-specified upon the command line for strenth.
+The B<passwd-test> binary is designed to test the passwords
+contained within the file(s) specified upon  the command line
+for strenth.
 
 Passwords will be tested using the cracklib testing library,
 and any weak passwords will be reported to the console.
