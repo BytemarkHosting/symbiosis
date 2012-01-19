dde9cd88f4396ea5dcd30f0b0886fc675f55519b
Patrick J Cherry <patrick@bytemark.co.uk>
2012-01-19 11:14:05 +0000

    Monit tests now installed with the rest.  The process handling has been improved.

--- monit/lib/symbiosis/monitor/process.rb
+++ monit/lib/symbiosis/monitor/process.rb
@@ -4,43 +4,66 @@ module Symbiosis
       class Process
 
         attr_accessor :initscript
-        attr_writer :pidfile
+        attr_writer :pidfile, :name
 
         def initialize
           @pidfile = nil
           @name = nil
-          @pid = nil
           @initscript = nil
           @sleep = 30
         end
 
         def pid
-          return @pid unless @pid.nil?
-          raise "pidfile not set" if @pidfile.nil?
-          @pid = File.open(@pidfile,'r'){|fh| fh.read}.chomp
+          raise ArgumentError, "pidfile not set" if @pidfile.nil?
+          begin
+            #
+            # Try to read the pidfile
+            #
+            pid = File.open(@pidfile,'r'){|fh| fh.read}.chomp
+            #
+            # Sanity check the PID found.
+            #
+            raise ArgumentError, "Bad PID in #{@pidfile}" unless pid =~ /^\d+$/
+            return pid
+
+          rescue Errno::ENOENT
+            #
+            # pidfile missing...
+            #
+            return nil
+          end
         end
         
         def name
-          return @name unless @name.nil?
-          raise "Cannot find pid" if self.pid.nil?
+          pid = self.pid
+          raise "Cannot find pid" if pid.nil?
 
-          File.open(File.join("", "proc", self.pid.to_s, "status"), 'r') do |fh|
-            while @name.nil? do
-              @name = $1 if fh.gets =~ /^Name:\s+(.*)$/
-              break if fh.eof?
-            end
-          end
-          @name
+          statusfile = File.join("", "proc", pid, "status")
+
+          #
+          # Raise a no-such-process error if the status file doesn't exist.
+          #
+          raise Errno::ESRCH, self.pid unless File.exists?(statusfile)
+
+          #
+          # Read the status file and find the name.
+          #
+          name = nil
+          File.readlines(statusfile, 'r').find{|l| l.chomp =~ /^Name:\s+(.*)$/ }
+          name = $1 unless $1.nil?
+
+          raise Errno::ESRCH, self.pid if name.nil?
+
+          name
         end
         
         def start
-          @pid = nil
           do_initscript("start")
           @sleep.times do
             begin
-              sleep 1
               break unless self.pid.nil?
-            rescue
+              sleep 1
+            rescue ArgumentError, Errno::ESRCH
               # do nothing.. We're only going to do this a maximum of @sleep
               # times.
             end
@@ -48,27 +71,24 @@ module Symbiosis
         end
 
         def stop
-          pid_test = true
-          begin
-            self.pid
-          rescue
-            # Hmmm.. We couldn't get the current PID.  So we're not going to be
-            # able to do the PID test later. 
-            pid_test = false
-          ensure
-            do_initscript("stop")
-          end
+          do_initscript("stop")
 
           @sleep.times do
             begin
               # check the PID but do nothing. We're only going to do this a
-              # maximum of @sleep times.
+              # maximum of @sleep times.  Programme has stopped if the PID is
+              # nil.
+              #
+              break if self.pid.nil?
               sleep 1
-              break if stop_test and self.pid.nil?
-            rescue
+            rescue ArgumentError, Errno::ESRCH
               break
             end
           end 
+        end
+
+        def is_running?
+
         end
 
         def check_initscript
@@ -76,16 +96,22 @@ module Symbiosis
           raise Errno::EPERM,  initscript unless File.executable?(initscript)
         end
 
+        #
+        # Run the initscript
+        #
         def do_initscript(action)
           return unless 0 == ::Process.uid
           check_initscript
-          @pid = nil
           Kernel.system("#{initscript} #{action} 2>&1")
         end
 
+        #
+        # This checks to make sure the PID matches an expected name.
+        #
         def do_check(n)
-          raise Errno::ESRCH, "PID #{self.pid} appears to be #{self.name}, not #{n}." unless self.name == n
+          raise Errno::ESRCH, self.pid unless n == self.name
         end
+
       end
     end
 end
