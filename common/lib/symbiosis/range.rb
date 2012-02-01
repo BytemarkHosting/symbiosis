7144d828cb7bf97669d89da02e6d79a6baeebcd4
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-01 12:02:18 +0000

    Updated the Symbiosis::Range api docs.

--- common/lib/symbiosis/range.rb
+++ common/lib/symbiosis/range.rb
@@ -1,6 +1,11 @@
 require 'symbiosis/host'
 
 module Symbiosis
+
+  #
+  # This class has been superceded by Symbiosis::Host, and will be removed in
+  # the future releases of Symbiosis.
+  #
   class Range < Host ; end
 end
 
