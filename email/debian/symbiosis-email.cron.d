cd94281145f714029cfe5b5a2828fbd421b5d7bb
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-01 20:45:09 +0000

    Removed duff cronjob.

--- email/debian/symbiosis-email.cron.d
+++
@@ -1,7 +0,0 @@
-#
-# Crontab snippet to scan /srv for new symlinks and writes a config file for
-# exim for domain rewrites.
-#
-
-*/1 * * * * Debian-exim [ -x /usr/sbin/exim_rewrite_scan ] && /usr/sbin/exim_rewrite_scan  /etc/exim4/exim4.conf
-
