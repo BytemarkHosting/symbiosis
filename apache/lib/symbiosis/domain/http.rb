ff5fd4a91ed28a5d9135bf406c7c0a298e3f7cbe
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-01 10:58:01 +0000

    Added Symbiosis::Domain#should_have_stats? to symbiosis-httpd

---
+++ apache/lib/symbiosis/domain/http.rb
@@ -0,0 +1,45 @@
+require 'symbiosis/domain'
+
+module Symbiosis
+
+  class Domain
+    #
+    # Checks to see if a domain should have statistics generated for it.
+    # Returns true if statistics should be generated, false if not.
+    #
+    def should_have_stats?
+      bool = get_param("no-stats", self.config_dir)
+
+      #
+      # We invert the flag, since it is called "no-stats".
+      #
+      return true if false == bool or bool.nil?
+
+      #
+      # Return false if the flag exists at all.
+      #
+      return false
+    end
+
+    #
+    # Sets the domain to have statistics generated, or not.  Expects true if
+    # statistics are wanted, or false if not.
+    #
+    def should_have_stats=(bool)
+      return ArgumentError, "Expecting true or false" unless [TrueClass, FalseClass].include?(bool.class)
+
+      #
+      # Invert the setting, since it is called "no-stats"
+      #
+      if true == bool
+        set_param("no-stats", false, self.config_dir)
+      else 
+        set_param("no-stats", true, self.config_dir)
+      end
+
+      return bool
+    end
+
+  end
+
+end
