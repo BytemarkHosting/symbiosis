12bd054ed68437e76930f1c4a74379f2a976608b
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-01 12:35:53 +0000

    Updated Symbiosis::Domain SSL API docs.

--- common/lib/symbiosis/domain/ssl.rb
+++ common/lib/symbiosis/domain/ssl.rb
@@ -2,18 +2,13 @@ require 'symbiosis/domain'
 require 'openssl'
 require 'erb'
 
-#
-# A helper class which copes with SSL-domains.
-#
-#
 module Symbiosis
 
   class Domain
 
     #
-    # Is SSL enabled for the domain?
-    #
-    # SSL is enabled if there is a matching key and certificate.
+    # Returns true if SSL has been enabled.  SSL is enabled if there is a
+    # matching key and certificate found using ssl_find_matching_certificate_and_key.
     #
     def ssl_enabled?
       self.ssl_x509_certificate and self.ssl_key
@@ -26,6 +21,11 @@ module Symbiosis
       get_param("ssl-only", self.config_dir)
     end
 
+    #
+    # Searches for the domain's SSL certificate using
+    # ssl_find_matching_certificate_and_key, and returns the certificate's filename, or
+    # nil if nothing could be found.
+    # 
     def ssl_x509_certificate_file
       @ssl_x509_certificate_file ||= nil
       
@@ -38,6 +38,9 @@ module Symbiosis
 
     alias ssl_certificate_file ssl_x509_certificate_file
 
+    #
+    # Sets the domains SSL certificate filename.
+    #
     def ssl_x509_certificate_file=(f)
       @ssl_x509_certificate_file = f
     end
@@ -55,6 +58,11 @@ module Symbiosis
       end
     end
 
+    #
+    # Searches for the domain's SSL key using
+    # ssl_find_matching_certificate_and_key, and returns the key's filename, or
+    # nil if nothing could be found.
+    #
     def ssl_key_file
       @ssl_key_file ||= nil
 
@@ -65,13 +73,16 @@ module Symbiosis
       @ssl_key_file
     end
 
+    #
+    # Sets the domains's SSL key filename.
+    #
     def ssl_key_file=(f)
       @ssl_key_file = f
     end
 
-
     #
-    # Returns the RSA key object
+    # Returns the domains SSL key as an OpenSSL::PKey::RSA object, or nil if no
+    # key file could be found.
     #
     def ssl_key
       unless self.ssl_key_file.nil?
@@ -82,7 +93,8 @@ module Symbiosis
     end
 
     #
-    # Returns the certificate chain file, if one exists, or one has been set.
+    # Returns the certificate chain filename, if one exists, or one has been
+    # set, or nil if nothing could be found.
     #
     def ssl_certificate_chain_file
       if get_param("ssl.bundle", self.config_dir)
@@ -95,7 +107,7 @@ module Symbiosis
     alias ssl_bundle_file ssl_certificate_chain_file
 
     #
-    # Add a path with extra SSL certs for testing
+    # Add a path with extra SSL certs (for testing).
     #
     def ssl_add_ca_path(path)
       @ssl_ca_paths ||= [] 
@@ -104,7 +116,12 @@ module Symbiosis
     end
 
     #
-    # Returns the X509 certificate store, including any specified chain file.
+    # Sets up and returns a new OpenSSL::X509::Store.
+    #
+    # If any CA paths have been set using ssl_add_ca_path, then these are added to the store.
+    #
+    # If ssl_certificate_chain_file has been set, then this is added to the store.
+    #
     # This is regenerated on every call.
     #
     def ssl_certificate_store
@@ -119,9 +136,18 @@ module Symbiosis
     end
 
     #
-    # Return the available certificate/key files
+    # Return the available certificate/key files for a domain.  It will check
+    # files with the following extensions for both keys and certificates.
+    #  * combined
+    #  * key
+    #  * crt
+    #  * pem
     #
-    # It will return an array [[certificate_files] , [key_files]]
+    # It will return an array of certificate and key filenames that could be
+    # read and parsed successfully by OpenSSL.  The array has to sub-arrays,
+    # the first being certificate filenames, the second key filenames, i.e.
+    # <code>[[certificates] , [keys]]</code>.  If a file contains both a
+    # certificate and key, it will appear in both arrays.
     #
     def ssl_available_files
       certificate_files = []
@@ -189,10 +215,16 @@ module Symbiosis
       [certificate_files, key_files]
     end
     
+    #
+    # This returns an array of files for the domain that contain valid certificates.
+    #
     def ssl_available_certificate_files
       self.ssl_available_files.first
     end
 
+    # 
+    # This returns an array of files for the domain that contain valid keys.
+    #
     def ssl_available_key_files
       self.ssl_available_files.last
     end
@@ -201,6 +233,9 @@ module Symbiosis
     # Tests each of the available key and certificate files, until a matching
     # pair is found.  Returns an array of [certificate filename, key_filename],
     # or nil if no match is found.
+    #
+    # The order in which keys and certficates are matched is determined by
+    # ssl_available_files.
     #
     def ssl_find_matching_certificate_and_key
       #
@@ -230,12 +265,25 @@ module Symbiosis
       return nil
     end
 
-    def ssl_verify
+    #
+    # This method performs a variety of checks on an SSL certificate and key:
+    #
+    # * Is the certificate valid for this domain name, or this domain with
+    #   "www." prepended.
+    # * Has the certificate started yet?
+    # * Has the certificate expired?
+    # * Does the key match the certificate?
+    # * If the certificate is not self-signed, does it need a bundle?
+    #
+    # If any of these checks fail, a OpenSSL::X509::CertificateError is raised.
+    #
+    def ssl_verify(certificate = self.ssl_x509_certificate, key = self.ssl_key, store = self.ssl_certificate_store)
+
       #
       # Firstly check that the certificate is valid for the domain.
       #
-      unless OpenSSL::SSL.verify_certificate_identity(self.ssl_x509_certificate, self.name) or
-             OpenSSL::SSL.verify_certificate_identity(self.ssl_x509_certificate, "www.#{self.name}")
+      unless OpenSSL::SSL.verify_certificate_identity(certificate, self.name) or
+             OpenSSL::SSL.verify_certificate_identity(certificate, "www.#{self.name}")
 
         raise OpenSSL::X509::CertificateError, "The certificate subject is not valid for this domain."
       end
@@ -243,18 +291,18 @@ module Symbiosis
       # Check that the certificate is current
       # 
       #
-      if self.ssl_x509_certificate.not_before > Time.now 
+      if certificate.not_before > Time.now 
         raise OpenSSL::X509::CertificateError, "The certificate is not valid yet."
       end
 
-      if self.ssl_x509_certificate.not_after < Time.now 
+      if certificate.not_after < Time.now 
         raise OpenSSL::X509::CertificateError, "The certificate has expired."
       end
 
       # Next check that the key matches the certificate.
       #
       #
-      unless self.ssl_x509_certificate.check_private_key(self.ssl_key)
+      unless certificate.check_private_key(key)
         raise OpenSSL::X509::CertificateError, "The certificate's public key does not match the supplied private key."
       end
      
@@ -264,15 +312,15 @@ module Symbiosis
       # First see if we can verify it using our own private key, i.e. the
       # certificate is self-signed.
       #
-      if self.ssl_x509_certificate.verify(self.ssl_key)
+      if certificate.verify(key)
         puts "\tUsing a self-signed certificate." if $VERBOSE
 
       #
       # Otherwise see if we can verify it using the certificate store,
       # including any bundle that has been uploaded.
       #
-      elsif self.ssl_certificate_store.verify(self.ssl_x509_certificate)
-        puts "\tUsing certificate signed by #{self.ssl_x509_certificate.issuer.to_s}" if $VERBOSE
+      elsif store.verify(certificate)
+        puts "\tUsing certificate signed by #{certificate.issuer.to_s}" if $VERBOSE
 
       #
       # If we can't verify -- raise an error.
