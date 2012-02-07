ef4f149eb48195ab8e8f07b299b0d6d052a0fc3b
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-07 14:51:29 +0000

    Updated symbiosis-make-logs to make webalizer logs with a template.

---
+++ apache/lib/symbiosis/config_files/webalizer.rb
@@ -0,0 +1,14 @@
+require 'symbiosis/config_file'
+require 'tempfile'
+
+module Symbiosis
+  module ConfigFiles
+    class Webalizer < Symbiosis::ConfigFile
+      # This is just a standard config.
+      #
+      # TODO: Need an OK? check.
+    end
+  end
+end
+
+
