49ee68ffe4d9b29d356b2996e9ed05747f1d3da9
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-08 11:09:17 +0000

    Updated webalizer config generation to take place in the config dir.

--- apache/lib/symbiosis/config_files/webalizer.rb
+++ apache/lib/symbiosis/config_files/webalizer.rb
@@ -21,8 +21,58 @@ module Symbiosis
 
         super
       end
+
+      #
+      # This method returns the location of the Webalizer "history" file, used
+      # to keep the last 12 months-worth of stats, for use in the HistoryName
+      # directive.
+      #
+      # Older machines will have it at public/htdocs/stats/webalizer.hist,
+      # newer ones at config/.webalizer.hist.
+      #
+      # It will default to the new location if nothing is found at the old
+      # location.
+      #
+      def history_name
+        #
+        # This is the old place.
+        #
+        old_location = File.join(domain.stats_dir, "webalizer.hist")
+        return old_location if File.exists?(old_location)
+
+        #
+        # OK, just use the new one.
+        #
+        return File.join(domain.config_dir, ".webalizer.hist")
+      end
+
+      #
+      # This method returns the location of the Webalizer "current" file, used
+      # for incremental stats, for use in the IncrementalName directive.
+      #
+      # Older machines will have it at public/htdocs/stats/webalizer.current,
+      # newer ones at config/.webalizer.current.
+      #
+      # It will default to the new location if nothing is found at the old
+      # location.
+      #
+      def incremental_name
+        #
+        # This is the old place.
+        #
+        old_location = File.join(domain.stats_dir, "webalizer.current")
+        return old_location if File.exists?(old_location)
+
+        #
+        # OK, just use the new one.
+        #
+        return File.join(domain.config_dir, ".webalizer.current")
+      end
+
     end
+
   end
+
 end
 
 
