0ac1845ab6425cd27ddabf43b98589d8fabcbf3d
Patrick J Cherry <patrick@bytemark.co.uk>
2012-02-22 16:22:22 +0000

    Fixed firewall tests to work.

--- firewall/test.d/tc_blacklistdb.rb
+++ firewall/test.d/tc_blacklistdb.rb
@@ -17,7 +17,7 @@ class TestBlacklistDb < Test::Unit::TestCase
 
   def teardown
     unless $DEBUG
-      File.unlink(@fn) if File.exists?(@fn)
+      File.unlink(@fn.path) if File.exists?(@fn.path)
     else
       FileUtils.move(@fn.path, @fn.path+"-saved")
       puts "BlacklistDB saved in #{@fn.path}-saved"
