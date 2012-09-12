57156ae5d9b7b1c4c8ab9cf65e14af16d25846af
Patrick J Cherry <patrick@bytemark.co.uk>
2012-09-12 11:03:08 +0100

    Updated FTP login tests.  Now both plain and crypted passwords are tested, and the quota tests pass.

--- ftpd/test.d/tc_ftp.rb
+++ ftpd/test.d/tc_ftp.rb
@@ -30,6 +30,8 @@ class TestFTP < Test::Unit::TestCase
   end
 
   def test_login
+    password_file = File.join(@domain.config_dir, "ftp-password")
+
     #
     # Try logging in when no password has been set.
     #
@@ -42,7 +44,7 @@ class TestFTP < Test::Unit::TestCase
     #
     # Try logging in when an empty file is in place.
     #
-    @domain.ftp_password = true
+    FileUtils.touch(password_file)
 
     assert_raise(Net::FTPPermError, "FTP Login succeeded when FTP logins were not permitted.")  do
       Net::FTP.open('localhost') do |ftp|
@@ -50,43 +52,61 @@ class TestFTP < Test::Unit::TestCase
       end
     end
 
-
     #
     #  Set the password to a random string.
     #
-    @domain.ftp_password = Symbiosis::Utils.random_string()
+    password = Symbiosis::Utils.random_string()
 
     #
-    # Try logging in without a password.
+    # Now test logins with both crypted and plain text passwords stored
     #
-    assert_raise(Net::FTPPermError, "FTP Login without password succeeded")  do
-      Net::FTP.open('localhost') do |ftp|
-        ftp.login( @domain.name, "" )
+    [
+      ["plain", password],
+      ["crypt'd", "{CRYPT}"+password.crypt("$1$"+Symbiosis::Utils.random_string(8)+"$")]
+    ].each do |crypted, password_data|
+
+      File.open(password_file,"w+") do |fh|
+        fh.puts(password_data)
+      end
+
+      #
+      # Try logging in without a password.
+      #
+      assert_raise(Net::FTPPermError, "FTP Login without password succeeded. Password stored #{crypted}.")  do
+        Net::FTP.open('localhost') do |ftp|
+          ftp.login( @domain.name, "" )
+        end
+      end
+
+      #
+      # Try logging in with an incorrect password.
+      #
+      assert_raise(Net::FTPPermError, "FTP Login with incorrect password succeeded.  Password stored #{crypted}.")  do
+        Net::FTP.open('localhost') do |ftp|
+          ftp.login( @domain.name, password+" BAD PASSWORD")
+        end
+      end
+
+      #
+      #  Attempt a login, and report on the success.
+      #
+      assert_nothing_raised("FTP Login with correct password failed.  Password stored #{crypted}.") do
+        Net::FTP.open('localhost') do |ftp|
+          ftp.login( @domain.name, password )
+        end
       end
     end
 
-    #
-    # Try logging in with an incorrect password.
-    #
-    assert_raise(Net::FTPPermError, "FTP Login with incorrect password succeeded")  do
-      Net::FTP.open('localhost') do |ftp|
-        ftp.login( @domain.name, @domain.ftp_password+" BAD PASSWORD")
-      end
-    end
-
-    #
-    #  Attempt a login, and report on the success.
-    #
-    assert_nothing_raised("FTP Login with correct password failed") do
-      Net::FTP.open('localhost') do |ftp|
-        ftp.login( @domain.name, @domain.ftp_password )
-      end
-    end
   end
 
 
   def test_quota
     quota_file = File.join(@domain.config_dir,"ftp-quota")
+
+    #
+    # A password is required, or the quota is always nil.
+    #
+    @domain.ftp_password = Symbiosis::Utils.random_string()
 
     [[1e6, "1M\n"],
      [2.5e9, "2.5G\n"],
