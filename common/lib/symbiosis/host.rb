5968a2a23f4a42399a9d947891246bf2e8972e0c
Patrick J Cherry <patrick@bytemark.co.uk>
2012-09-18 15:42:07 +0100

    Added new Bytemark IP range 5.153.224.0/21

--- common/lib/symbiosis/host.rb
+++ common/lib/symbiosis/host.rb
@@ -10,7 +10,7 @@ module Symbiosis
   #
   class Host 
 
-    BYTEMARK_RANGES = %w(80.68.80.0/20 89.16.160.0/19 212.110.160.0/19 46.43.0.0/18 91.223.58.0/24 213.138.96.0/19 2001:41c8::/32).collect{|i| IPAddr.new(i)}
+    BYTEMARK_RANGES = %w(80.68.80.0/20 89.16.160.0/19 212.110.160.0/19 46.43.0.0/18 91.223.58.0/24 213.138.96.0/19 5.153.224.0/21 2001:41c8::/32).collect{|i| IPAddr.new(i)}
 
     BACKUP_SPACE_FILENAME = "/etc/symbiosis/dns.d/backup.name"
 
