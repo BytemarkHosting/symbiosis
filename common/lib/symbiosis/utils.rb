5cb991ad74625552c984fd191681548bb37ee677
Steve Kemp <steve@steve.org.uk>
2013-09-23 16:06:24 +0100

    Added show_usage(), show_help(), show_help_or_usage().

--- common/lib/symbiosis/utils.rb
+++ common/lib/symbiosis/utils.rb
@@ -8,6 +8,57 @@ module Symbiosis
   #
   module Utils
 
+
+    #
+    # Many of our utility scripts have integrated documentation at their
+    # head.
+    #
+    # This method will show the manual to the caller.
+    #
+    def show_manual( filename )
+      show_help_or_usage( filename, false )
+    end
+
+    #
+    # Many of our utility scripts have integrated documentation at their
+    # head.
+    #
+    # This method will show brief usage-information to the caller.
+    #
+    def show_usage( filename )
+      show_help_or_usage( filename, true )
+    end
+
+
+    #
+    #  Show either the manual, or the brief usage text.
+    #
+    def show_help_or_usage( filename, help )
+
+      #
+      # Open the file, stripping the shebang line
+      #
+      lines = File.open(filename){|fh| fh.readlines}[1..-1]
+
+      found_synopsis = false
+
+      lines.each do |line|
+
+        line.chomp!
+        break if line.empty?
+
+        if help and !found_synopsis
+          found_synopsis = (line =~ /^#\s+SYNOPSIS\s*$/)
+          next
+        end
+
+        puts line[2..-1].to_s
+        break if help and found_synopsis and line =~ /^#\s*$/
+      end
+
+    end
+
+
     # 
     # This function uses the FileUtils mkdir_p command to make a directory.
     # It adds the extra options of :uid and :gid to allow these to be set in
@@ -419,7 +470,7 @@ module Symbiosis
       raise Errno::ENOLCK, "Unable to release lock -- #{err.to_s}"
     end
 
-    module_function :mkdir_p, :set_param, :get_param, :random_string, :safe_open, :parse_quota, :lock, :unlock
+    module_function :mkdir_p, :set_param, :get_param, :random_string, :safe_open, :parse_quota, :lock, :unlock, :show_usage, :show_help, :show_help_or_usage
 
   end
 
