9b792b67e0a554cb080621b5ea7b958166a76c4c
Patrick J Cherry <patrick@bytemark.co.uk>
2013-10-22 12:46:06 +0100

    Added dkim support to the common library

---
+++ common/lib/symbiosis/domain/dkim.rb
@@ -0,0 +1,131 @@
+require 'symbiosis/domain'
+require 'openssl'
+require 'base64'
+require 'erb'
+
+module Symbiosis
+
+  class Domain
+
+    #
+    # Returns true if DKIM public and private keys are available, and match.
+    #
+    def dkim_enabled?
+      self.dkim_selector and self.dkim_key
+    end
+
+    #
+    # Returns the domains SSL key as an OpenSSL::PKey::RSA object, or nil if no
+    # key file could be found.
+    #
+    def dkim_key
+      return @dkim_key unless @dkim_key.nil?
+      key = get_param("dkim.key", self.config_dir)
+
+      @dkim_key = if key.is_a?(String)
+        begin
+          OpenSSL::PKey::RSA.new(key)
+        rescue OpenSSL::OpenSSLError => err
+          nil
+        end
+      else
+        nil
+      end
+    end
+
+    #
+    # Returns the domains SSL key as an OpenSSL::PKey::RSA object, or nil if no
+    # key file could be found.
+    #
+    def dkim_key=(k)
+      raise ArgumentError, "key is not an OpenSSL::PKey::RSA" unless k.is_a?(OpenSSL::PKey::RSA)
+      @dkim_key = k
+    end
+
+    #
+    # Returns the public part of the DKIM key, or nil of no DKIM key is available
+    #
+    def dkim_public_key
+      if self.dkim_key.is_a?(OpenSSL::PKey::RSA)
+        self.dkim_key.public_key
+      else
+        nil
+      end
+    end
+
+    #
+    # Generates a DKIM private key.
+    #
+    def generate_dkim_key
+      self.dkim_key = OpenSSL::PKey::RSA.new(1536)
+    end
+
+    #
+    # This returns the dkim selector, stored in config/dkim.  If that file is
+    # empty, the first component of either /etc/mailname, /etc/hostname, or the
+    # hostname returned by the hostname(1) command is used.
+    #
+    def dkim_selector
+      selector = get_param("dkim", self.config_dir)
+
+      @dkim_selector = if selector.is_a?(String) and selector =~ /^[A-Za-z0-9.-]+$/
+        selector
+
+      elsif true === selector
+        #
+        # Try /etc/mailname
+        #
+        hostname = get_param("mailname", '/etc')
+
+        #
+        # Failing that, try /etc/hostname
+        #
+        unless hostname.is_a?(String)
+          hostname = get_param("hostname", '/etc')
+        end
+
+        #
+        # Fall back to a command, if needed.
+        #
+        unless hostname.is_a?(String)
+          hostname = `hostname`.chomp
+        end
+
+        #
+        # Take the first availble hostname, and default to "default" if empty.
+        #
+        hostname = hostname.to_s.split($/).first.strip
+        hostname = "default" if hostname.empty?
+        hostname
+
+      else
+        nil
+
+      end
+    end
+
+    #
+    # This returns the Base64 encoded public DKIM key for use in a TXT record.
+    #
+    def dkim_public_key_txt
+      #
+      # Ruby 1.8 uses a different output format for the DER encoded public key
+      # to both OpenSSL and Ruby1.9+, so we have to construct the correct
+      # format ourselves.
+      #
+      der_key = if RUBY_VERSION =~ /^1\.8/ 
+        OpenSSL::ASN1::Sequence.new([
+          OpenSSL::ASN1::Sequence.new([
+            OpenSSL::ASN1::ObjectId.new("rsaEncryption"),
+            OpenSSL::ASN1::Null.new(nil)
+          ]),
+          OpenSSL::ASN1::BitString.new(self.dkim_public_key.to_der)
+        ]).to_der
+      else
+        self.dkim_public_key.to_der
+      end
+
+      Base64::encode64(der_key).gsub("\n","")
+    end
+  end
+end
---
+++ common/test.d/tc_dkim.rb
@@ -0,0 +1,105 @@
+$:.unshift  "../lib/" if File.directory?("../lib")
+
+require 'test/unit'
+require 'tmpdir'
+require 'symbiosis/domain/dkim'
+
+class DkimTest < Test::Unit::TestCase
+
+  def setup
+    @prefix = Dir.mktmpdir("srv")
+    @prefix.freeze
+    @domain = Symbiosis::Domain.new(nil, @prefix)
+    @domain.create
+  end
+
+  def teardown
+    unless $DEBUG
+      @domain.destroy  if @domain.is_a?( Symbiosis::Domain)
+      FileUtils.rm_rf(@prefix) if File.directory?(@prefix)
+    end
+  end
+
+  def rsa_private_key_pem
+  <<EOF
+-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgQCuLPyxujsFxJj5ZmvNPsk88kCTsq71/HkwBw+F3IJUjfKUgakX
+o9y60qzCqyUauro9gYdKcstwr+5nIDKlAAn5cyTiNgDqOLc5ROZ2s/hIfB4/P9qj
++kENhWYovEIRi6kuCGVEtTLKc0OboNrFUQ0r40FJrGdVsVMB3cRcF0mVgQIDAQAB
+AoGAIM8Wln/nCFIdIrWZTuMp0xIq+edpr6psRZC+6s87uaO3cyPtbyeNt59hrZXB
+eoR7+oQAsRRooARz2vcksxILzqKc4K/OGrrAv8eCJMWjBNKqc8sgI5vyHNxj7DN5
+7+0LL5MY3g+CMSSDmfnHavfE3sR+vfPLxDs5yH2o6c8t6iUCQQDg6bb+cVotf3R2
+GL4IEBumv2YbEpMOLufAX5c8DyoB3g5rQfoOmcQogtnrjjea78qAbrvh2OlkrnRk
+k4buzADnAkEAxj/+jkmwKynyYfoH5FZHoeUshAdR481zC+jhmZ6lcwrgm8fhB1od
+hhEFHeOWYCmlSubokTlWhopjY3h4QPyhVwJABuNBZmNkRpZro544W5jar+2Wm+ei
+t0F6eWqz//Pa7nm1aVV46e+NkUwIjm0piMYlJm+9sznoU9v/1oCqFjALKwJAHoES
+RgqIlNurc+/o7vVnqD1/EAGgVBD0tsxqihyjEISH8vBaa6suB8bupp6yMLG3wUKu
+XkoYSjNY/6E1v6ofmQJARctrCu4TVpu3kf9UHbmTDvORTEZVwf8QNxbuWuxQ4q6N
+zft9X7eB5Lxw67aY+AeKmZlV8uor1+pkrBgUmwsY6Q==
+-----END RSA PRIVATE KEY-----
+EOF
+  end
+
+  def rsa_public_key_pem
+  <<EOF
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuLPyxujsFxJj5ZmvNPsk88kCT
+sq71/HkwBw+F3IJUjfKUgakXo9y60qzCqyUauro9gYdKcstwr+5nIDKlAAn5cyTi
+NgDqOLc5ROZ2s/hIfB4/P9qj+kENhWYovEIRi6kuCGVEtTLKc0OboNrFUQ0r40FJ
+rGdVsVMB3cRcF0mVgQIDAQAB
+-----END PUBLIC KEY-----
+EOF
+  end
+
+  def rsa_public_key_txt
+    "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuLPyxujsFxJj5ZmvNPsk88kCTsq71/HkwBw+F3IJUjfKUgakXo9y60qzCqyUauro9gYdKcstwr+5nIDKlAAn5cyTiNgDqOLc5ROZ2s/hIfB4/P9qj+kENhWYovEIRi6kuCGVEtTLKc0OboNrFUQ0r40FJrGdVsVMB3cRcF0mVgQIDAQAB"
+  end
+
+  def rsa_private_key
+    OpenSSL::PKey::RSA.new(rsa_private_key_pem)
+  end
+
+  def rsa_public_key
+    OpenSSL::PKey::RSA.new(rsa_public_key_pem)
+  end
+
+  #####
+  #
+  # Tests
+  #
+  #####
+
+  def test_dkim_selector
+    @domain.__send__(:set_param,"dkim", false, @domain.config_dir)
+    assert_equal(nil, @domain.dkim_selector)
+
+    @domain.__send__(:set_param,"dkim", true, @domain.config_dir)
+
+    #
+    # This should return (in order) the first component of mailname, hostname,
+    # or # $(hostname)
+    #
+    if File.exists?('/etc/mailname')
+      hostname = File.read("/etc/mailname")
+    elsif File.exists?('/etc/hostname')
+      hostname = File.read("/etc/hostname")
+    else
+      hostname = `hostname`
+    end
+
+    hostname = hostname.to_s.split($/).first.strip
+    hostname = "default" if hostname.empty?
+
+    assert_equal(hostname, @domain.dkim_selector)
+
+    @domain.__send__(:set_param,"dkim", "foo", @domain.config_dir)
+    assert_equal("foo", @domain.dkim_selector)
+  end
+
+  def test_dkim_public_key
+    @domain.__send__(:set_param,"dkim.key", rsa_private_key_pem, @domain.config_dir)
+    assert_equal(rsa_private_key_pem, @domain.dkim_key.to_pem)
+    assert_equal(rsa_public_key_txt, @domain.dkim_public_key_txt)
+  end
+
+end
