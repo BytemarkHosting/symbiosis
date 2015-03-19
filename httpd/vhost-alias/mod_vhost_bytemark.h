9796b5bac0818cd1a654966de402302a09716da3
Steve Kemp <steve@steve.org.uk>
2015-03-19 16:41:12 +0000

    Updated how we mangle paths.
    I suspect I've identified a problem with the way that the path
    operations were carried out that will *only* manifest itself when
    symlinks are used heavily.
    
    The specific problem is that we get passed a buffer of the form:
    
        /srv/example.com/path/to/request.php
    
    If example.com is a symlink to a hostname with a different length
    then the pointer-arithmatic is wrong.
    
    To resolve this problem we've updated things to explicitly parse
    the incoming reqest into three pieces:
    
        /srv/
    
        $hostname
    
        $request
    
    Then all further stats are carried out on the three parts individulally,
    we have to take a copy of `$request` to avoid self-corruption, but this
    is a small price to pay.
    
    Closes #10258

--- httpd/vhost-alias/mod_vhost_bytemark.h
+++ httpd/vhost-alias/mod_vhost_bytemark.h
@@ -62,13 +62,12 @@
  * We'll be passed a path to a request, on disk, which doesn't exist
  * such as: /srv/www.example.com/public/htdocs
  *
- * We want toremove components of that name until we find something
+ * We want to remove components of that name until we find something
  * that does exist, if we can.
  *
  * If we cannot find something which exists, by removing components
  * from the hostname field, then we'll simply return the string
  * unmodified - which will allow Apache to handle it as-is.
- *
  *
  * NOTE: We can always successfully remove string-components in-place
  *      as this always *reduces* the string in length.
@@ -80,7 +79,6 @@ void update_vhost_request( char *path )
   char *per = NULL;
   struct stat statbuf;
   int i;
-  int host;
 
 
   /**
@@ -110,20 +108,40 @@ void update_vhost_request( char *path )
   /**
    * Log the missing request.
    */
-#if 0
+#if VHOST_DEBUG
   fprintf(stderr,"mod_vhost_bytemark.c: path not found %s\n", path);
 #endif
 
+
   /**
-   * OK at this point we have a request which points to a file
-   * which doesn't exist.
+   * OK at this point we have a request which points to a file which
+   * doesn't exist.
    *
    * So we might have:
    *
    *   /srv/www.example.com/public/htdocs/index.php
    *
-   * We take the first component of that string "www.example.com"
-   * and will remove successive parts over time.
+   * This string is made up of three parts;
+   *
+   *  - The static prefix: "/srv/".
+   *
+   *  - The hostname: "www.example.com".
+   *
+   *  - The requested resource: "/public/htdocs/index.php".
+   *
+   * We don't know if the file exists, but we can test if the hostname
+   * is hosted locally by looking for /srv/$hostname.
+   *
+   * We will stat the hostname, looking at each sub-string, because
+   * we want to look for common suffixes.  For example:
+   *
+   *   www.example.com -> example.com
+   *
+   *   sub.dom.example.com -> example.com
+   *
+   * This means we're looking for the right-most match and that can be
+   * discovered by iterating over the length of the hostname and trying
+   * each one.
    *
    * If _any_ of those result in a stat() succeeding we update
    * and return.
@@ -136,8 +154,9 @@ void update_vhost_request( char *path )
 
 
   /**
-   * OK we want to find the string after /srv/
-   * but before the first slash
+   * OK we want to find the hostname.
+   *
+   * The hostnaem is the string after /srv/, but before the first slash.
    */
   per = strstr( path + strlen("/srv/" ), "/" );
   if ( per == NULL )
@@ -145,22 +164,45 @@ void update_vhost_request( char *path )
 
 
   /**
-   * We want to bump-past the slash.
+   * At this point we can calculate the host-length.
    */
-  per +=1;
+  int host_len = per - path - 5  /* strlen("/srv/" ); */ ;
+  if ( host_len >= 128 )
+  {
+    fprintf(stderr,"mod_vhost_bytemark.c: hostname too long: %d bytes\n", host_len);
+    return;
+  }
 
 
   /**
-   * The length of the hostname in the request.
+   * So now we have a hostname - save it away in the host_name buffer.
    */
-  host = per - srv - strlen("/srv/");
-  if ( host > 128 )
-  {
-#if 0
-    fprintf(stderr,"mod_vhost_bytemark.c: hostname too long: %d bytes\n",host);
+  char host_name[256];
+  memset( host_name, '\0', sizeof(host_name)-1);
+  strncpy( host_name,
+           path + strlen("/srv/" ),
+           per - path - 5  /* strlen("/srv/" ); */  );
+
+
+  //#define VHOST_DEBUG 1
+#ifdef VHOST_DEBUG
+  printf( "XXX: Incoming Request was: %s\n", path );
+  printf( "     Hostname - %s [%d]\n", host_name, host_len );
+  printf( "     Path  - %s\n", per );
 #endif
-    return;
-  }
+
+
+  /**
+   * So now we have:
+   *
+   *  1.  The hostname stored in "host_name"  (www.foo.com)
+   *  2.  The hostname length stored in "host_len" (11)
+   *  3.  The requested path stored in "per" ("/index.php").
+   *
+   * Take a copy of the path that is requested.
+   */
+  char *path_copy = strdup( per );
+  int path_len = strlen( per );
 
 
 
@@ -168,38 +210,70 @@ void update_vhost_request( char *path )
    * Try increasingly stat()ing over parts of the hostname
    * until we find a match.
    */
-  for ( i = 1; i < host; i++ )
+  for ( i = 1; i < host_len - 1; i++ )
   {
-    char buffer[1024];
 
-    /**
-     * Build up each part of the name.
-     */
-    memset(buffer, '\0', sizeof(buffer));
-    strcpy(buffer,"/srv/" );
-    strncpy( buffer+5,srv+4 + i, host - i );
+     /**
+      * We know the hostname cannot be more than 128 bytes, so we're
+      * safe to declare this as 256.
+      */
+     char buffer[256];
+
+     /**
+      * Add the /srv/ + domain-piece prefix.
+      */
+     memset(buffer, '\0', sizeof(buffer));
+     strcpy(buffer,"/srv/" );
+     strncpy(buffer + 5, host_name + i, host_len - i );
 
 
-    /**
-     * If we can successfully state "/srv/" + $hostname
-     * then we'll update the string with that name.
-     */
-    if ( stat( buffer, &statbuf) == 0 )
-    {
-      memcpy( path+5, srv+4+i, strlen(srv+4+i)+1 );
-#if 0
-      fprintf(stderr,"mod_vhost_bytemark.c: succeeded on %s -> %s\n",buffer, path);
+     /**
+      * If we can successfully state "/srv/" + $hostname
+      * then we'll update the string with that name.
+      */
+     if ( stat( buffer, &statbuf) == 0 )
+     {
+        memcpy( path+5, srv+4+i, strlen(srv+5+i) );
+
+#ifdef VHOST_DEBUG
+        fprintf(stderr,"mod_vhost_bytemark.c: succeeded on %s -> %s\n",buffer, path);
 #endif
-      return;
+
+        /**
+         * Zero the RAM we were given.
+         */
+        memset(path, '\0', path_len );
+
+        /**
+         * Add the hostname we statted.
+         */
+        strcpy( path, buffer );
+
+        /**
+         * Add the request - which will start with "/".
+         */
+        strcat(path, path_copy );
+
+        /**
+         * Free the copy of the requested-path we took.
+         */
+        free(path_copy);
+        return;
     }
-  }
+
+#ifdef VHOST_DEBUG
+     fprintf(stderr, "failed to stat: %s\n", buffer );
+#endif
+}
 
   /**
    * Failure -> 404.
    */
-#if 0
+#ifdef VHOST_DEBUG
   fprintf(stderr,"mod_vhost_bytemark.c: giving up\n" );
 #endif
+
+  free(path_copy);
 }
 
 
