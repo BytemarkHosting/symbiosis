b3b1c636dc29a61bb24f8fa37503b0f778a7e027
Steve Kemp <steve@steve.org.uk>
2015-04-08 16:34:00 +0100

    Started to move towards using unattended-upgrades.
    We've removed the local-files here, and have now started to
    depend upon the package that will provide the new implementation.
    
    TODO: Deploy sane configuration files for u-a.

--- updater/debian/symbiosis-updater.cron.d
+++
@@ -1,7 +0,0 @@
-#
-# Update the system, for new Symbiosis packages and security updates.
-# 
-# This should happen between 9.30 and 10.30 (given a random sleep), Monday to
-# Friday.
-
-30 9 * * 1-5 root [ -x /usr/sbin/symbiosis-updater ] && /usr/sbin/symbiosis-updater
