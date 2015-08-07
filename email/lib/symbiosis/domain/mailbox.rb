7431784a9a3c28ddeb57c9e546ddb96e8c27ec42
Patrick J Cherry <patrick@bytemark.co.uk>
2015-08-07 22:20:18 +0100

    email: Moved new "should_encrypt_passwords" into the main mailbox class.

--- email/lib/symbiosis/domain/mailbox.rb
+++ email/lib/symbiosis/domain/mailbox.rb
@@ -506,6 +506,17 @@ module Symbiosis
       quota
     end
 
+    #
+    # Returns true if this domain has email password encryption enabled.
+    #
+    def should_encrypt_mailbox_passwords?
+      if get_param("mailbox-dont-encrypt-passwords",self.config_dir)
+        false
+      else
+        true
+      end
+    end
+
   end
 
   class Domains
