9bb84649f90674292756ce21b32884c6ace1bfc9
Patrick J Cherry <patrick@bytemark.co.uk>
2015-08-21 10:10:45 +0100

    webmail: Updated symbiosis roundcube tweaks to reflect the new jessie config

--- webmail/roundcube/main.symbiosis.inc.php
+++ webmail/roundcube/main.symbiosis.inc.php
@@ -2,17 +2,15 @@
 /*****************************************************************************
  *
  * This configuration is deployed with Bytemark Symbiosis.  Feel free to make
- * changes here, or to the original configuration in
- * /etc/roundcube/main.inc.php.dpkg-symbiosis-orig.
+ * changes here.
  *
  ****************************************************************************/
 
-
-/**
- * Read in original, main config.
+/*
+ * Make sure the configuration array is defined.
  */
-if (is_readable('/etc/roundcube/main.inc.php.dpkg-symbiosis-orig')) {
-  include_once('/etc/roundcube/main.inc.php.dpkg-symbiosis-orig');
+if ! is_array( $rcmail_config ) {
+  $rcmail_config = array();
 }
 
 /**
@@ -29,9 +27,7 @@ if ( !array_key_exists('default_host', $rcmail_config) or
  * Make sure the plugins array exists.
  */
 if ( !array_key_exists('plugins', $rcmail_config) ) {
-
   $rcmail_config['plugins'] = array();
-
 }
 
 /**
@@ -50,4 +46,20 @@ if ( array_search("managesieve", $rcmail_config['plugins']) === false ) {
    * http://wiki2.dovecot.org/Pigeonhole/Sieve/Troubleshooting)
    */
   $rcmail_config['managesieve_mbox_encoding'] = 'UTF-8';
+
+  /* Enables separate management interface for vacation responses (out-of-office)
+   * 0 - no separate section (default),
+   * 1 - add Vacation section,
+   * 2 - add Vacation section, but hide Filters section
+   */
+  $rcmail_config['managesieve_vacation'] = 1;
+
 }
+
+if ( !array_key_exists('force_https', $rcmail_config) ) {
+  /*
+   * Make sure all connections are secure. 
+   */
+  $rcmail_config['force_https'] = true;
+}
+
