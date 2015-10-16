09de86054f4ace4bd1147964ad7d70e0228e8603
Patrick J Cherry <patrick@bytemark.co.uk>
2015-10-16 12:30:26 +0100

    email: Added poppassd to allow roundcube (et al) to change passwords

---
+++ email/lib/symbiosis/email/poppass_handler.rb
@@ -0,0 +1,137 @@
+require 'eventmachine'
+require 'em/protocols/line_protocol'
+require 'symbiosis/domains'
+require 'symbiosis/domain/mailbox'
+require 'syslog'
+require 'cracklib'
+
+module Symbiosis
+  module Email
+    class PoppassHandler < EM::Connection
+
+      def initialize(fail_timeout = 3)
+        @fail_timeout = fail_timeout
+      end
+
+      def self.prefix=(p)
+        @@prefix = p
+      end
+
+      def self.syslog=(s)
+        @@syslog = s
+      end
+ 
+      include EventMachine::Protocols::LineProtocol
+    
+#    C: <open connection to port 106>
+#    S: 200 poppassd (version 1.1) ready.
+#    C: USER fred
+#    S: 300 send password
+#    C: PASS stupid
+#    S: 200 Hello, fred.
+#    C: NEWPASS still-stupid
+#    S: 200 Password changed for fred
+#    C: QUIT
+#    S: Goodby.
+#
+#    C: <open connection to port 106>
+#    S: 200 poppassd (version 1.1) ready.
+#    C: USER fred
+#    S: 300 send password
+#    C: PASS stupid
+#    S: 500 Incorrect login.
+#    C: QUIT
+#    S: Goodby.
+
+      def post_init
+        send_data "200 Hello, who are you?\r\n"
+        @user = @mailbox = nil
+        @authorised = false
+      end
+
+      def receive_line(l)
+        case l
+          when /^USER (.+)$/i
+            @username = $1
+            @authorised = false
+            send_data "300 Please send your current password\r\n"
+            
+          when /^PASS (.+)$/i
+            @authorised = false
+            ans = do_authorise($1)
+            send_data ans
+
+          when /^NEWPASS (.+)$/i
+            ans = do_change_password($1)
+            send_data ans
+
+          when /^QUIT/i
+            send_data "200 ta-ra\r\n"
+            close_connection(true)
+
+          else
+            send_data "500 I don't understand what you're saying.\r\n"
+        
+        end
+      rescue StandardError => err
+        send_data "500 Something bad has happened.  Sorry!\r\n"
+        syslog.warning "Caught #{err.to_s}"
+        close_connection(true)
+      end
+
+      def syslog
+        @@syslog
+      end
+
+      def prefix
+        @@prefix
+      end
+
+      def do_authorise(passwd)
+
+        unless @username
+          return "400 Please send your username first!\r\n"
+        end
+
+        @mailbox = Symbiosis::Domains.find_mailbox(@username, self.prefix)
+
+        if @mailbox.nil?
+          syslog.info "Non-existent mailbox #{@username.inspect}"
+          sleep @fail_timeout
+          return "500 Incorrect login\r\n"
+        end
+
+        begin
+          @authorised = @mailbox.login(passwd)
+        rescue ArgumentError => err
+          syslog.info "Unable to login to mailbox #{@username.inspect} because #{err.to_s}"
+          sleep @fail_timeout
+          return "500 Incorrect login\r\n"
+        end
+
+        unless @authorised
+          syslog.info "Incorrect password given for mailbox #{@username.inspect}"
+          sleep @fail_timeout
+          return "500 Incorrect login\r\n"
+        end
+
+        return "200 Nice to see you again, #{@username}\r\n"
+      end
+
+      def do_change_password(passwd)
+        unless @authorised
+          return "400 Please login before trying to change your password\r\n"
+        end
+
+        c = CrackLib::Fascist(passwd)
+
+        unless c.ok?
+          return "400 Sorry, that password is too weak -- #{c.reason}\r\n"
+        end
+
+        @mailbox.password = passwd
+        return "200 Password changed\r\n"
+      end
+    end
+  end
+end
---
+++ email/symbiosis/test.d/tc_poppassd.rb
@@ -0,0 +1,175 @@
+require 'symbiosis/email/poppass_handler'
+require 'socket'
+require 'test/unit'
+require 'tmpdir'
+require 'tempfile'
+require 'eventmachine'
+require 'syslog'
+require 'socket'
+
+class TestEmailPoppassClient < EventMachine::Connection
+  attr_reader :script, :result
+
+  def initialize(q,r)
+    @script = q
+    @result = r
+  end
+
+  def receive_data(l)
+    @result << l.chomp
+
+    unless script.empty?
+      msg = script.shift
+      send_data(msg+"\r\n") 
+    end
+  end
+
+  def unbind
+    EM.stop
+  end
+
+end
+
+class TestEmailPoppassd < Test::Unit::TestCase
+
+  def setup
+    @prefix = Dir.mktmpdir("srv")
+    File.chown(1000,1000,@prefix)
+
+    @domains = []
+
+    @syslog = Syslog.open( File.basename($0), Syslog::LOG_NDELAY, Syslog::LOG_MAIL )
+
+    Symbiosis::Email::PoppassHandler.prefix = @prefix
+    Symbiosis::Email::PoppassHandler.syslog = @syslog
+    @sock = File.join(@prefix, File.basename($0)+"-#{$$}.sock")
+    
+    domain = Symbiosis::Domain.new(nil, @prefix)
+    domain.create
+    @mailbox = domain.create_mailbox("aarvo")
+  end
+
+  def teardown
+    #
+    #  Delete the temporary domain
+    #
+    @domains.each{|d| d.destroy() unless d.nil? }
+
+    #
+    # Remove the @prefix directory
+    #
+    FileUtils.remove_entry_secure @prefix
+
+    @syslog.close
+  end
+
+  #
+  # Not sure if this is a good thing to do.
+  #
+  def eventmachine(timeout = 30)
+    Timeout::timeout(timeout) do
+      #
+      # Catch all eventmachine errors
+      #
+      EM.error_handler{ |e|
+        flunk (["Error raised during EM loop: #{e.message}: #{e.to_s}"]+e.backtrace).join("\n")
+      }
+
+      EM.epoll
+      EM.run do
+        yield
+      end
+    end
+  rescue Timeout::Error
+    flunk 'Eventmachine was not stopped before the timeout expired'
+  end
+
+  require 'stringio'
+
+  def do_test_script(script)
+    result = []
+    self.eventmachine do
+      server  = EM.start_server("localhost", 0, Symbiosis::Email::PoppassHandler, 0)
+      port    = Socket.unpack_sockaddr_in( EM.get_sockname( server )).first
+      client  = EM.connect("localhost", port, TestEmailPoppassClient, script, result)
+    end
+
+    return result
+  end
+
+  def test_login_and_change
+    @mailbox.password = "abc"
+    assert @mailbox.login("abc")
+
+    new_password = " this is a super secure password."
+
+    results = do_test_script(["USER #{@mailbox.username}", "PASS abc", "NEWPASS #{new_password}", "QUIT"])
+
+    # we should get back 200 (hello), 300 (please log in), 200 (authd), 200 (changed), 200 (bye)
+    expected_results = [/^2\d\d /, /^3\d\d /, /^2\d\d/, /^2\d\d /, /^2\d\d /]
+
+    results.zip(expected_results).each do |r, e|
+      assert_match e, r
+    end
+
+    # now see if we can log in.
+    assert(!@mailbox.login("abc"))
+    assert(@mailbox.login(new_password))
+  end
+
+  def test_login_with_empty_password
+    results = do_test_script(["USER #{@mailbox.username}", "PASS abc","QUIT"])
+
+    # we should get back 200 (hello), 300 (please log in), 500 (failed), 200 (bye)
+    expected_results = [/^2\d\d /, /^3\d\d /, /^5\d\d/, /^2\d\d /]
+
+    results.zip(expected_results).each do |r, e|
+      assert_match e, r
+    end
+
+  end
+  
+  def test_login_with_nonexistent_user
+    results = do_test_script(["USER nobody@#{@mailbox.domain.name}", "PASS abc","QUIT"])
+
+    # we should get back 200 (hello), 300 (please log in), 500 (failed), 200 (bye)
+    expected_results = [/^2\d\d /, /^3\d\d /, /^5\d\d/, /^2\d\d /]
+
+    results.zip(expected_results).each do |r, e|
+      assert_match e, r
+    end
+  end
+
+  def test_change_with_no_login
+    @mailbox.password = "abc"
+
+    results = do_test_script(["USER nobody@#{@mailbox.domain.name}", "NEWPASS abc", "QUIT"])
+    # we should get back 200 (hello), 300 (password please), 400 (temp fail), 200 (bye)
+    expected_results = [/^2\d\d /, /^3\d\d/, /^4\d\d /, /^2\d\d /]
+
+    results.zip(expected_results).each do |r, e|
+      assert_match e, r
+    end
+  end
+
+  def test_weak_pasword
+    @mailbox.password = "abc"
+    assert @mailbox.login("abc")
+
+    new_password = "typewriter"
+
+    results = do_test_script(["USER #{@mailbox.username}", "PASS abc", "NEWPASS #{new_password}", "QUIT"])
+
+    # we should get back 200 (hello), 300 (please log in), 200 (authd), 400 (temp fail), 200 (bye)
+    expected_results = [/^2\d\d /, /^3\d\d /, /^2\d\d/, /^4\d\d /, /^2\d\d /]
+
+    results.zip(expected_results).each do |r, e|
+      assert_match e, r
+    end
+
+    # now make sure the password hasn't changed.
+    assert(@mailbox.login("abc"))
+    assert(!@mailbox.login(new_password))
+  end
+
+end
