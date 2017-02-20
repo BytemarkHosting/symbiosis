dc3692eaad282664b098fc3f798fdffe7dee53d4
Patrick J Cherry <patrick@bytemark.co.uk>
2017-02-20 18:53:50 +0000

    common: Use environment to determine hostname.
    Not sure this is a good idea, but it might make the tests pass..

--- common/lib/symbiosis/host.rb
+++ common/lib/symbiosis/host.rb
@@ -27,6 +27,8 @@ module Symbiosis
     # like junk, "localhost" is returned.
     #
     def self.fqdn
+			return ENV['HOSTNAME'] if ENV['HOSTNAME']
+
       #
       # Start by working out our localhost name
       #
