6bd6f2be505c41fd2235b5742f456ec2d0be6042
telyn <telyn@bytemark.co.uk>
2017-08-11 17:14:31 +0100

    Implement symbiosis-ssl hooks

--- common/lib/symbiosis/ssl.rb
+++ common/lib/symbiosis/ssl.rb
@@ -1,5 +1,17 @@
 module Symbiosis
   class SSL
     PROVIDERS ||= []
+
+    def self.call_hooks(domains_with_updates)
+      return if domains_with_updates.empty?
+
+      Dir.glob('/etc/symbiosis/ssl-hooks.d/*').each do |script|
+        next unless File.executable?(script)
+        IO.popen([script, 'live-update'], 'r+') do |io|
+          io.puts domains_with_updates.join("\n")
+          io.close_write # Close the pipe now we've written stuff.
+        end
+      end
+    end
   end
 end
