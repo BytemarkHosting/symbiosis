9d87d56a50e7b80f4fec0855ce46090b91610db2
telyn <telyn@bytemark.co.uk>
2017-08-29 19:59:57 +0100

    rewrite hooks into a nicer class, add documentation, etc.

--- common/lib/symbiosis/ssl.rb
+++ common/lib/symbiosis/ssl.rb
@@ -5,31 +5,5 @@ module Symbiosis
   # SSL knows about which SSL providers exist and provides SSL helper functions
   class SSL
     PROVIDERS ||= []
-
-    def self.call_hooks(domains_with_updates, event)
-      hooks_path = Symbiosis.path_in_etc('/symbiosis/ssl-hooks.d/*')
-      success = true
-
-      Dir.glob(hooks_path).each do |script|
-        next unless File.executable?(script)
-        next unless File.basename(script) =~ /^[a-zA-Z0-9_-]+$/
-
-        success &&= run_hook_script(event, script, domains_with_updates)
-      end
-      success
-    end
-
-    def self.run_hook_script(event, script, domains_with_updates)
-      opts = { stdin_data: domains_with_updates.join("\n") }
-      output, status = Open3.capture2e([script, event], opts)
-
-      return true if status.success?
-
-      puts "============================================\n"
-      puts "Error executing SSL script for #{event} hook\n"
-      puts "#{script} exited with status #{status.exitstatus}\n"
-      puts output + "\n\n"
-      false
-    end
   end
 end
