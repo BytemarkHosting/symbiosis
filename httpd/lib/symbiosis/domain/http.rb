f2f830820379090be8f89b7850841c9121de6323
telyn <telyn@bytemark.co.uk>
2017-08-31 10:01:37 +0100

    Switch out no-stats config file for a stats config file - false by default

--- httpd/lib/symbiosis/domain/http.rb
+++ httpd/lib/symbiosis/domain/http.rb
@@ -8,17 +8,7 @@ module Symbiosis
     # Returns true if statistics should be generated, false if not.
     #
     def should_have_stats?
-      bool = get_param("no-stats", self.config_dir)
-
-      #
-      # We invert the flag, since it is called "no-stats".
-      #
-      return true if false == bool or bool.nil?
-
-      #
-      # Return false if the flag exists at all.
-      #
-      return false
+      get_param('stats', self.config_dir) != false
     end
 
     #
@@ -28,16 +18,7 @@ module Symbiosis
     def should_have_stats=(bool)
       return ArgumentError, "Expecting true or false" unless [TrueClass, FalseClass].include?(bool.class)
 
-      #
-      # Invert the setting, since it is called "no-stats"
-      #
-      if true == bool
-        set_param("no-stats", false, self.config_dir)
-      else 
-        set_param("no-stats", true, self.config_dir)
-      end
-
-      return bool
+      set_param("stats", bool, self.config_dir)
     end
 
     #
